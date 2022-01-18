601e0976e30bcbed0cd355caadc8d011cee611ab
benbd5 <belzbenjaminp@gmail.com>
2022-01-19 00:20:26 +0100

    Protection des routes

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -2,17 +2,19 @@ const router = require('express').Router()
 
 const sessionController = require('../controller/sessionController')
 
+const withAuth = require('../middlewares/authMiddleware')
+
 router.get('/sessions', sessionController.getAllSessions)
 router.get('/session/:id', sessionController.getOneSession)
 
 router.get('/sport-values', sessionController.getSports)
 
-router.post('/add', sessionController.postSession)
+router.post('/add', withAuth, sessionController.postSession)
 
-router.patch('/edit_session/:id', sessionController.patchSession)
+router.patch('/edit_session/:id', withAuth, sessionController.patchSession)
 
 router.patch('/member/:id', sessionController.memberOfSession)
 
-router.delete('/delete_session', sessionController.deleteSession)
+router.delete('/delete_session', withAuth, sessionController.deleteSession)
 
 module.exports = router
